126615be89cf5f01fdffcd9719c817c9f4137518
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-17 22:30:50 +0200

    added Astroonaut struct

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by MaćKo on 17/04/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
