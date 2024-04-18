d9b17376db976d2b1d7c2bc25ff4930cbcea0062
Maciej Kostrzycki <maciej.kostrzycki@gmail.com>
2024-04-18 11:07:02 +0200

    added mission view with mission details

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,45 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by MaćKo on 18/04/2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+}
